c8ade7cbdc9e93c9e415c87e9935394388e6cd2b
Sobhan <sobhan.saf79@gmail.com>
2020-09-19 10:16:20 +0430

    index file

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Autos Database</title>
+    <link rel="stylesheet" href="./style/style.css" />
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to Autos Database</h1>
+        <a href="login.php">Please Log In</a>
+        <p>This is just a test page!</p>
+        <p>Other pages (you should login first):</p>
+        <ul>
+            <li><a href="view.php">View added autos</a></li>
+            <li><a href="add.php">Add a new car</a></li>
+        </ul>
+    </div>
+</body>
+</html>
