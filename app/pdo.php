6eedbcd9a95761376c752c3a75e4d7b08880e4e5
sobhan.saf79 <sobhan.saf79@gmail.com>
2022-05-11 00:12:26 +0430

    changed pdo.php

--- app/pdo.php
+++ app/pdo.php
@@ -1,5 +1,5 @@
 <?php
-$dsn = "mysql:dbname=misc;host=localhost";
-$pdo = new PDO($dsn, 'autos_user', '123456');
+$dsn = "mysql:dbname=" . getenv("db_name") .";host=" . getenv("db_host");
+$pdo = new PDO($dsn, getenv("db_user"), getenv("db_pass"));
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
